e9d3902f48c16b20b420b371e3ce250110417bc5
zeimao77 <zeimao77@foxmail.com>
2021-07-20 09:11:11 +0800

    1

---
+++ cdslib/defc.h
@@ -0,0 +1,10 @@
+#ifndef __DEFC_H__
+#define __DEFC_H__
+
+const int INT_MAX = 0x7FFFFFFF;
+const int INT_MIN = 0x80000000;
+
+const long LONG_MAX = 0x7FFFFFFFFFFFFFFF;
+const long LONG_MIN = 0x8000000000000000;
+
+#endif
